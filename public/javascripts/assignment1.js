08515d72278e6bfb7e9f79fe1af95f8e4a64668f
jarodmalczynski <jarodmalczynski@users.noreply.github.com>
2018-02-07 14:41:19 -0500

    Initial commit

---
+++ public/javascripts/assignment1.js
@@ -0,0 +1,9 @@
+/*
+ * Design the logic for a program for the River Falls Homes Construction Company.
+ * Design a program that prompts the user for a lot number in the River Falls subdivision,
+ * and data about the home to be built there including number of bedrooms, number of bathrooms,
+ * and size of garage in number of cars it holds. Output is the price of the home which is $50,000
+ * base price plus $17,000 for each bedroom, $12,500 for each bathroom, and $6,000 for each car the
+ * garage holds. (Only needs to run once).
+ */
+
